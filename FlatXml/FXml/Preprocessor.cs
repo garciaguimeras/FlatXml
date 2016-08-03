22c3ef0c4ec75c7346a69160196accc0eef9dfa0
Noel Garcia Guimeras <garcia.guimeras@gmail.com>
2016-08-02 21:53:24 -0400

    Adding the macro @ for meta attributes

--- FlatXml/FXml/Preprocessor.cs
+++ FlatXml/FXml/Preprocessor.cs
@@ -6,14 +6,39 @@ namespace FlatXml.FXml
 	public class Preprocessor
 	{
 
+		private string ProcessMetaAttributeMacro(string line)
+		{
+			if (!line.StartsWith("@"))
+				return line;
+
+			string name = "";
+			string value = "";
+
+			line = line.Substring(1);
+			int pos = line.IndexOf(" ");
+			if (pos == -1)
+				name = line;
+			else
+			{
+				name = line.Substring(0, pos);
+				value = line.Substring(pos + 1).Trim();
+				value = value.Replace("\"", "'");
+			}
+			return string.Format("meta-attr name=\"{0}\" value=\"{1}\"", name, value);
+		}
+
 		public string FixString(List<string> lines)
 		{
 			string code = "";
 
 			foreach (string line in lines)
 			{
-				code = code + line.Trim() + TokenParser.NEWLINE;
+				string str = line.Trim();
+				str = ProcessMetaAttributeMacro(str);
+				str = str + TokenParser.NEWLINE;
+				code = code + str;
 			}
+
 			code = code + " " + TokenParser.STOP;
 			return code;
 		}
